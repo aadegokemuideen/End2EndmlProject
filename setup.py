81c683862d1bd9ec17cc2e1f1e5ae752defee26c
aadegokemuideen <aadegokemuideen@yahoo.com>
2023-03-19 11:32:03 +0000

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+from typing import List
+
+'''
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Adegoke Muideen",
+    author_email="aadegokemuideen@yahoo.com",
+    packages=find_packages(),
+    install_requires = [
+    "pandas", 
+    "numpy",
+    "seaborn"
+    ]
+)
+
+'''
+
+HYPEN_E_DOT = '-e .'
+def  get_requirements(file_path:str)->List[str]:
+    """
+    This function will return the list of requiremnts
+
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements =[req.replace('\n',"") for req in requirements if req != HYPEN_E_DOT]
+
+    return requirements
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Adegoke Muideen",
+    author_email="aadegokemuideen@yahoo.com",
+    packages=find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
