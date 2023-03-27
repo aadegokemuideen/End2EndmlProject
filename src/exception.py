03967baa4952920d5735b5838390debb80e04ae8
aadegokemuideen <aadegokemuideen@yahoo.com>
2023-03-27 08:23:17 +0100

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 ## This is customized exceptions
 import sys
 import logging
-import logger
+#import logger 
 
-def error_message_detail(error, error_detail:sys):
+def error_message_detail(error, error_detail:str):
     _,_,exc_tb = error_detail.exc_info() #give 3 information
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message ="Error occured in python script name [{0}] line number [{1}] error message[{2}]". \
